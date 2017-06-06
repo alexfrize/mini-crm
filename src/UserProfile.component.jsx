0be507f664dd1c64edfa08388074429167c9dae7
Alexander Frize <alexander.frize@gmail.com>
2017-06-05 17:16:51 -0700

    Added profile

--- src/UserProfile.component.jsx
+++ src/UserProfile.component.jsx
@@ -14,44 +14,59 @@ export default class UserProfile extends Component {
 	render() {
 		return (
 			<div className="UserProfile">
-				<h2 className="h2">Progress:</h2>
-				<div className="ProgressItem">
-					<div className="ProgressItem__img-and-text">
-						<img className="ProgressItem__img" src={progress__newLead} alt="" />
-						<p className="ProgressItem__img-caption">New lead</p>
-					</div>
+				<div className="UserProfile__progress">
+					<h2 className="h2">Progress:</h2>
+					<div className="ProgressItem">
+						<div className="ProgressItem__img-and-text">
+							<img className="ProgressItem__img" src={progress__newLead} alt="" />
+							<p className="ProgressItem__img-caption">New lead</p>
+						</div>
 
-					<textarea className="ProgressItem__comment-textarea" />
-				</div>
-				<div className="ProgressItem">
-					<div className="ProgressItem__img-and-text">
-						<img className="ProgressItem__img" src={progress__phoneCall} alt="" />
-						<p className="ProgressItem__img-caption">Phone call</p>
+						<textarea className="ProgressItem__comment-textarea" placeholder= "Enter description here..." />
 					</div>
-					<textarea className="ProgressItem__comment-textarea" />					
+					<div className="ProgressItem">
+						<div className="ProgressItem__img-and-text">
+							<img className="ProgressItem__img" src={progress__phoneCall} alt="" />
+							<p className="ProgressItem__img-caption">Phone call</p>
+						</div>
+						<textarea className="ProgressItem__comment-textarea" />					
 
+					</div>
+					<div className="ProgressItem">
+						<div className="ProgressItem__img-and-text">
+							<img className="ProgressItem__img" src={progress__presentation} alt="" />
+							<p className="ProgressItem__img-caption">Presentation</p>
+						</div>
+						<textarea className="ProgressItem__comment-textarea" />					
+					</div>	
+					<div className="ProgressItem">
+						<div className="ProgressItem__img-and-text">
+							<img className="ProgressItem__img" src={progress__contract} alt="" />
+							<p className="ProgressItem__img-caption">Contract</p>
+						</div>
+						<textarea className="ProgressItem__comment-textarea" />					
+					</div>	
+					<div className="ProgressItem">
+						<div className="ProgressItem__img-and-text">
+							<img className="ProgressItem__img" src={progress__done} alt="" />
+							<p className="ProgressItem__img-caption">Done!</p>
+						</div>
+						<textarea className="ProgressItem__comment-textarea" />					
+					</div>	
 				</div>
-				<div className="ProgressItem">
-					<div className="ProgressItem__img-and-text">
-						<img className="ProgressItem__img" src={progress__presentation} alt="" />
-						<p className="ProgressItem__img-caption">Presentation</p>
-					</div>
-					<textarea className="ProgressItem__comment-textarea" />					
-				</div>	
-				<div className="ProgressItem">
-					<div className="ProgressItem__img-and-text">
-						<img className="ProgressItem__img" src={progress__contract} alt="" />
-						<p className="ProgressItem__img-caption">Contract</p>
-					</div>
-					<textarea className="ProgressItem__comment-textarea" />					
-				</div>	
-				<div className="ProgressItem">
-					<div className="ProgressItem__img-and-text">
-						<img className="ProgressItem__img" src={progress__done} alt="" />
-						<p className="ProgressItem__img-caption">Done!</p>
-					</div>
-					<textarea className="ProgressItem__comment-textarea" />					
-				</div>	
+
+				<div className="UserProfile__profile">
+					<h2 className="h2">Profile:</h2>
+					<form className="UserProfile__profile__form">
+						<input className="UserProfile__profile__form__input" placeholder="Name:"/>
+						<input className="UserProfile__profile__form__input" placeholder="E-mail:"/>
+						<input className="UserProfile__profile__form__input" placeholder="Phone:"/>
+						<textarea className="UserProfile__profile__form__textarea" placeholder="Description:" />
+						<div className="UserProfile__profile__form__button-container">
+							<button className="UserProfile__profile__form__button">Save</button>
+						</div>
+					</form>
+				</div>
 			</div>
 		);
 	}
