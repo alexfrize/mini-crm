b74f4011ebe06e492bd271055349332ff7c04094
Alexander Frize <alexander.frize@gmail.com>
2017-06-07 15:12:53 -0700

    Added datepicker

--- src/UserProfile.component.jsx
+++ src/UserProfile.component.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import './datepicker/datepicker.css';
+import moment from 'moment';
+
 import './UserProfile.component.css';
 import progress__newLead from './img/progress__new-lead.svg';
 import progress__phoneCall from './img/progress__phone-call.svg';
@@ -7,9 +11,56 @@ import progress__contract from './img/progress__contract.svg';
 import progress__done from './img/progress__done.svg';
 
 export default class UserProfile extends Component {
-	// constructor(props) {
-	// 	super(props);
-	// }
+	constructor(props) {
+		super(props);
+		this.state = {startDate: moment()};
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+
+	handleChange(date) {
+		this.setState({
+			startDate: date
+		});
+		console.log(date);
+	}
+
+	render_UserProfile__tasks__select() {
+		var options = [];
+		var val = new Array(4);
+		var am_pm = "";
+		var timeHour = 0;
+		for (let i=0; i<=23; i++) {
+			if (i < 12) {
+				am_pm = " AM";
+				timeHour = i;
+			}
+			else {
+				am_pm = " PM";
+				timeHour = i - 12; 
+			}
+			if (timeHour === 0) {
+				val[0] = "12:00" + am_pm;
+				val[1] = "12:15" + am_pm;
+				val[2] = "12:30" + am_pm;
+				val[3] = "12:45" + am_pm;
+			}
+			else {
+				val[0] = (timeHour >= 10) ? timeHour+":00" + am_pm : "0"+timeHour+":00" + am_pm;
+				val[1] = (timeHour >= 10) ? timeHour+":15" + am_pm : "0"+timeHour+":15" + am_pm;
+				val[2] = (timeHour >= 10) ? timeHour+":30" + am_pm : "0"+timeHour+":30" + am_pm;
+				val[3] = (timeHour >= 10) ? timeHour+":45" + am_pm : "0"+timeHour+":45" + am_pm;												
+			}
+			for (let c=0; c<4; c++) {
+				options.push(<option key={i+"0"+c} value={val[c]}>{val[c]}</option>);
+			}
+		}
+		return (
+			<select className="UserProfile__tasks__task__input">
+				{options}
+			</select>
+		);
+	}
 
 	render() {
 		return (
@@ -74,14 +125,11 @@ export default class UserProfile extends Component {
 						<h2 className="h2">Tasks:</h2>
 							<div className="UserProfile__tasks__task">
 								<div className="UserProfile__tasks__task__input-group">
-									<input className="UserProfile__tasks__task__input" placeholder="Name:"/>
-									<select className="UserProfile__tasks__task__input">
-									  <option value="12:00 AM">12:00 AM</option>
-									  <option value="12:30 AM">12:30 AM</option>
-									  <option value="01:00 AM">01:00 AM</option>
-									  <option value="01:30 AM">01:30 AM</option>
-									</select>
-
+									<DatePicker className="UserProfile__tasks__task__input"
+									    selected={this.state.startDate}
+									    onChange={this.handleChange}
+									/>
+									{this.render_UserProfile__tasks__select()}
 								</div>
 								<textarea className="UserProfile__tasks__task__task-description" placeholder="Task description:" />
 							</div>
