846b80140a6f662d4a25559e173a6a529f6a9315
Alexander Frize <alexander.frize@gmail.com>
2017-06-13 16:09:19 -0700

    Added progress scheme

--- src/UserProfile/UserProfileProgress.component.jsx
+++ src/UserProfile/UserProfileProgress.component.jsx
@@ -5,6 +5,7 @@ import progress__phoneCall from './img/progress__phone-call.svg';
 import progress__presentation from './img/progress__presentation.svg';
 import progress__contract from './img/progress__contract.svg';
 import progress__done from './img/progress__done.svg';
+import progress__arrow from './img/progress__arrow.svg';
 
 import './UserProfileProgress.component.css';
 
@@ -19,9 +20,12 @@ export default class UserProfileProgress extends Component {
 		super(props);
 		this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
 		this.saveProgressDescriptions = this.saveProgressDescriptions.bind(this);
+		this.activateNthProgressSchemeItem = this.activateNthProgressSchemeItem.bind(this);
 		var progressDescription = new Array(5).fill("");
+		var activeProgressStep = 0;
 		this.state = {
-			progressDescription : progressDescription
+			progressDescription : progressDescription,
+			activeProgressStep: activeProgressStep
 		};
 	}
 
@@ -56,6 +60,51 @@ export default class UserProfileProgress extends Component {
 		return (<div>{progressItemsJSX}</div>)
 	}
 
+	/* ============================================================================================================ */
+	activateNthProgressSchemeItem(i, event) {
+		console.log(i);
+		this.setState({activeProgressStep : i});
+	}
+
+	/* ============================================================================================================ */
+	getSchemeItemClass(i) {
+		const baseClassName = "UserProfile__progress__progress-scheme__item";
+		return (i <= this.state.activeProgressStep) ? 
+			baseClassName + " " + baseClassName + "_active" : 
+			baseClassName + " " + baseClassName + "_non-active";
+	
+	}
+
+	/* ============================================================================================================ */
+	renderProgressScheme() {
+		function exeptLastArrow(i) {
+			return (i === progressItems_static.images.length-1) ? "display-none" : "";
+		}
+
+		var scheme = [];
+		
+		for (let i=0; i < progressItems_static.images.length; i++) {
+			scheme.push(
+
+					<div key={"scheme"+i} className={this.getSchemeItemClass(i)}>
+						<div onClick={this.activateNthProgressSchemeItem.bind(this, i)} className="UserProfile__progress__progress-scheme__item__figure">
+							<img  className="UserProfile__progress__progress-scheme__item__figure__img" src={progressItems_static.images[i]} alt="" />
+							<p className="UserProfile__progress__progress-scheme__item__figure__text">{progressItems_static.captions[i]}</p>
+						</div>
+						<div className={exeptLastArrow(i)}>
+							<img className="UserProfile__progress__progress-scheme__item__arrow" src={progress__arrow} alt=""/>
+						</div>
+					</div>
+				);
+		}
+
+		return (
+				<div className="UserProfile__progress__progress-scheme">
+					{scheme}
+				</div>
+			);
+	}
+
 	/* ============================================================================================================ */	
 	render() { 
 
@@ -63,8 +112,9 @@ export default class UserProfileProgress extends Component {
 				<div className="UserProfile__progress">
 					<h2 className="h2">Progress:</h2>
 					
-					{this.renderProgressItems()}
+					{this.renderProgressScheme()}
 
+					{this.renderProgressItems()}
 
 					<div className="UserProfile__progress__form__button-container">
 						<button onClick={this.saveProgressDescriptions}className="UserProfile__progress__form__button">Save</button>
