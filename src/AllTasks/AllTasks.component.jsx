0488caefb68c2befd0a3ef44500292d5d8b48911
Alexander Frize <alexander.frize@gmail.com>
2017-06-19 15:23:48 -0700

    Added local JSON data loading and table sorting

--- src/AllTasks/AllTasks.component.jsx
+++ src/AllTasks/AllTasks.component.jsx
@@ -3,9 +3,95 @@ import './AllTasks.component.css';
 import allTasks__done from './img/all-tasks__done.svg';
 import allTasks__edit from './img/all-tasks__edit.svg';
 import allTasks__profile from './img/all-tasks__profile.svg';
+import moment from 'moment';
 
 export default class AllTasks extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			users : [],
+			tasks: []
+		};
+	}
+
+	/* ============================================================================================================ */	
+	componentWillMount() {
+		this.setState({tasks: this.getTasksArray()});
+	}
+
+	/* ============================================================================================================ */	
+	getTasksArray() {
+		var alltasks = [];
+		var taskObject = {};
+		for (let userNum=0; userNum < this.props.users.length; userNum++ ) {
+			for (let taskNum=0; taskNum < this.props.users[userNum].tasks.length; taskNum++) {
+				if (this.props.users[userNum].tasks[taskNum].description !== "") {
+					taskObject = {
+						task : this.props.users[userNum].tasks[taskNum],
+						user: this.props.users[userNum].profile
+					};
+					alltasks.push(taskObject);
+				}
+				else console.log(`User "${this.props.users[userNum].profile.name}" has an empty task!`)
+			}
+		}		
+		console.log("alltasks===",alltasks);
+
+		alltasks.sort((task1, task2) => {
+			let date1 = moment(task1.task.date, 'MM/DD/YYYY');
+			let date2 = moment(task2.task.date, 'MM/DD/YYYY');
+			if (moment(date1).isSame(date2)) {
+				let time1 = moment(task1.task.time, 'hh:mm');
+				let time2 = moment(task2.task.time, 'hh:mm');
+				if (moment(time1).isBefore(time2)){
+					return -1;
+					}
+				else {
+					return 1;
+
+				}
+			}
+			if (moment(date1).isBefore(date2))
+				return -1;
+			else return 1;
+		});
+
+		return alltasks;
+	}
+
+	/* ============================================================================================================ */	
+	componentDidMount() {
+		console.log("this.state.tasks===",this.state.tasks);
+	}
+
+	/* ============================================================================================================ */	
 	render() {
+		var tasksTable = [];
+		for (let i=0; i<this.state.tasks.length; i++) {
+			tasksTable.push(
+						<tr key={"tablerow"+i}>
+							<td>{this.state.tasks[i].task.time}</td>
+							<td>{this.state.tasks[i].task.date}</td>
+							<td>{this.state.tasks[i].task.description}
+								<table className="AllTasks__table__sub-table">
+									<tbody>
+										<tr>
+											<td>{this.state.tasks[i].user.name}</td>
+											<td>{this.state.tasks[i].user.phone}</td>
+											<td>{this.state.tasks[i].user.email}</td>
+										</tr>
+									</tbody>
+								</table>
+							</td>
+							<td>
+								<img className="AllTasks__table__img" src={allTasks__edit} alt="" />
+								<img className="AllTasks__table__img" src={allTasks__profile} alt="" />
+								<img className="AllTasks__table__img" src={allTasks__done} alt="" />
+							</td>
+						</tr>
+			)
+		}
+
 		return (
 			<div className="AllTasks">
 				<div className="AllTasks__header">
@@ -15,91 +101,9 @@ export default class AllTasks extends Component {
 					</div>
 				</div>
 				<table className="AllTasks__table">
-					<tr>
-						<td>12:00 PM</td>
-						<td>06/05/2017</td>
-						<td>Make a phone call to Long John. Talk about new
-							project in Los Angeles.Make a phone call to Long John. Talk about new
-							project in Los Angeles.Make a phone call to Long John. Talk about new
-							project in Los Angeles.Make a phone call to Long John. Talk about new
-							project in Los Angeles.
-							<table className="AllTasks__table__sub-table">
-								<tr>
-									<td>Lorem ipsum dolor</td>
-									<td>Lorem ipsum dolor</td>
-									<td>Lorem ipsum dolor</td>
-								</tr>
-							</table>
-						</td>
-						<td>
-							<img className="AllTasks__table__img" src={allTasks__edit} alt="" />
-							<img className="AllTasks__table__img" src={allTasks__profile} alt="" />
-							<img className="AllTasks__table__img" src={allTasks__done} alt="" />
-						</td>
-					</tr>
-					<tr>
-						<td>12:00 PM</td>
-						<td>06/05/2017</td>
-						<td>Make a phone call to Long John. Talk about new
-							project in Los Angeles.Make a phone call to Long John. Talk about new
-							project in Los Angeles.Make a phone call to Long John. Talk about new
-							project in Los Angeles.Make a phone call to Long John. Talk about new
-							project in Los Angeles.
-							<table className="AllTasks__table__sub-table">
-								<tr>
-									<td>Lorem ipsum dolor</td>
-									<td>Lorem ipsum dolor</td>
-									<td>Lorem ipsum dolor</td>
-								</tr>
-							</table>
-						</td>
-						<td>
-							<img className="AllTasks__table__img" src={allTasks__edit} alt="" />
-							<img className="AllTasks__table__img" src={allTasks__profile} alt="" />
-							<img className="AllTasks__table__img" src={allTasks__done} alt="" />
-						</td>
-					</tr>
-					<tr>
-						<td>12:00 PM</td>
-						<td>06/05/2017</td>
-						<td>Make a phone call to Long John. Talk about new
-							project in Los Ange a phone call to Long John. Talk about new
-							project in Los Angeles.
-							<table className="AllTasks__table__sub-table">
-								<tr>
-									<td>Lorem ipsum dolor</td>
-									<td>Lorem ipsum dolor</td>
-									<td>Lorem ipsum dolor</td>
-								</tr>
-							</table>
-						</td>
-						<td>
-							<img className="AllTasks__table__img" src={allTasks__edit} alt="" />
-							<img className="AllTasks__table__img" src={allTasks__profile} alt="" />
-							<img className="AllTasks__table__img" src={allTasks__done} alt="" />
-						</td>
-					</tr>
-					<tr>
-						<td>12:00 PM</td>
-						<td>06/05/2017</td>
-						<td>Make a phone call to Long John. Talk about new
-							project in Los Angeles.Make a phone call to Long John. Talk about new
-							project inphone call to Long John. Talk about new
-							project in Los Angeles.
-							<table className="AllTasks__table__sub-table">
-								<tr>
-									<td>Lorem ipsum dolor</td>
-									<td>Lorem ipsum dolor</td>
-									<td>Lorem ipsum dolor</td>
-								</tr>
-							</table>
-						</td>
-						<td>
-							<img className="AllTasks__table__img" src={allTasks__edit} alt="" />
-							<img className="AllTasks__table__img" src={allTasks__profile} alt="" />
-							<img className="AllTasks__table__img" src={allTasks__done} alt="" />
-						</td>
-					</tr>																								
+					<tbody>
+						{ tasksTable }																				
+					</tbody>
 				</table>
 
 			</div>
--- src/App.component.jsx
+++ src/App.component.jsx
@@ -3,13 +3,42 @@ import './App.component.css';
 import Menu from './Menu/Menu.component.jsx';
 import UserProfile from './UserProfile/UserProfile.component.jsx';
 import AllTasks from './AllTasks/AllTasks.component.jsx';
+import userJSON from "./data/users.json"; 
 
 class App extends Component {
+  constructor() {
+  	super();
+  	this.state = {
+  		users : [],
+  	};
+  }
+
+  /* ============================================================================================================ */	
+  componentWillMount() {
+  	this.setState({
+	  		users: userJSON,
+	  	});
+  }
+
+  /* ============================================================================================================ */	
+  componentDidMount() {
+	console.log("APP: _state === ", this.state.users);
+
+  	/*
+  	var _url = "./data/users.json"; 
+  	fetch(_url)
+  	.then((res) => res.json())
+  	.then((res) => console.log(res))
+  	.catch((err) => console.error("Error loading data from JSON: ", err));
+  	*/
+  }
+
+  /* ============================================================================================================ */	
   render() {
     return (
       <div className="App">
         <Menu />
-        <AllTasks />
+        <AllTasks users={this.state.users} />
         <UserProfile />
 
       </div>
